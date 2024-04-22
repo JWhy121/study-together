504b106f3c5da5c83e8472e9384a36378945c7d1
[Cloud3] 김연지 <yjuio8357@naver.com>
2024-04-23 03:58:21 +0900

    Initial commit

---
+++ src/test/java/com/elice/studytogether/StudyTogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elice.studytogether;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyTogetherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
