130c2ef86fde18f5dd0bc3a994a2ed75c6f5d02b
[Cloud3] 김연지 <yjuio8357@naver.com>
2024-04-29 02:34:04 +0900

    chor: mapper 설정

---
+++ src/main/java/com/elice/studytogether/dto/CommentResponseDto.java
@@ -0,0 +1,16 @@
+package com.elice.studytogether.dto;
+
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
+public class CommentResponseDto {
+    private Long id;
+    private Long postId;
+    private String content;
+
+}
